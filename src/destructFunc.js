3d5e29b1a616653b16ccba40189deae6f1f7a55f
Vladimir Kolosov <vovakolos99>
2023-03-29 22:17:43 +0500

    reload

--- src/destructFunc.js
+++ src/destructFunc.js
@@ -1,16 +1,10 @@
-function getDescription(item) {
-  if (item.description === undefined) {
-    return 'Описание недоступно';
-  }
-  return item.description;
-}
 
 export default function getSpecialArray(obj) {
   const listSpecial = obj.special;
   const specialArray = [];
   for (const item of listSpecial) {
     const {
-      id, name, icon, description = getDescription(item),
+      id, name, icon, description = item.description || "Отсутствует описание",
     } = item;
     const objItem = {
       id, name, icon, description,
--- src/orderByProps.js
+++ src/orderByProps.js
@@ -1,44 +1,22 @@
 function getProp(obj, n, l) {
-  const nameObj = {}; const
-    levelObj = {};
-  nameObj[n] = '';
+  const nameObj = {},
+        levelObj = {};
+  nameObj[n] = '',
   levelObj[l] = '';
-  const otherListPropSort = Object.keys(obj).sort();
-  const otherObj = {};
-  for (const key of otherListPropSort) {
+  const otherListPropSort = Object.keys(obj).sort(),
+        otherObj = {};
+  for (let key of otherListPropSort) {
     otherObj[key] = obj[key];
   }
   const mergePropObj = { ...nameObj, ...levelObj, ...otherObj };
   return mergePropObj;
 }
 
-function getPropV2(obj, n, l) {
-  const nameObj = {}; const levelObj = {}; const
-    otherObj = {};
-  nameObj[n] = '';
-  levelObj[l] = '';
-  for (const prop in obj) {
-    if (prop !== n || prop !== l) {
-      otherObj[prop] = obj[prop];
-    }
-  }
-  const otherObjSort = Object.keys(otherObj).sort().reduce((obj, key) => {
-    obj[key] = otherObj[key];
-    return obj;
-  }, {});
-  const mergePropObj = { ...nameObj, ...levelObj, ...otherObjSort };
-  return mergePropObj;
-}
+export default function orderByProps(obj, params) {
+  let mergePropObj = getProp(obj, params[0], params[1]);
 
-export default function orderByProps(obj, params, p = true) {
-  let mergePropObj;
-  if (p) {
-    mergePropObj = getProp(obj, params[0], params[1]);
-  } else {
-    mergePropObj = getPropV2(obj, params[0], params[1]);
-  }
   const listObj = [];
-  for (const item of Object.entries(mergePropObj)) {
+  for (let item of Object.entries(mergePropObj)) {
     const itemObj = { key: item[0], value: item[1] };
     listObj.push(itemObj);
   }
